4f1c73be5d26aeb1b01ada093d7312ee9e43c6da
Mckarc <michaelandre752@gmail.com>
2023-03-08 16:35:11 -0500

    Update index.html, style.css & app.js

--- app.js
+++ app.js
@@ -1,8 +1,77 @@
+// Select all IDs from the from
+const email = document.querySelector('#email');
+const username = document.querySelector('#username');
 const togglePassword = document.querySelector('#togglePassword');
 const password = document.querySelector('#password');
-
 const toggleConfirmPassword = document.querySelector('#toggleConfirmPassword');
 const confirmPassword = document.querySelector('#confirmPassword');
+const form = document.querySelector('#signUp');
+
+// Prevent the form from submitting
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+})
+
+// develop utility functions
+// field is required
+const isRequired = value => value === '' ? false : true;
+// length of a field is between min and max
+const isBetween = (length, min, max) => length < min || length > max ? false : true;
+// email is valid 
+const isEmailValid = (email) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+};
+// password is strong
+const isPasswordSecure = (password) => {
+    const re = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+    return re.test(password);
+};
+
+
+// Functions that show the error/ success message
+const showError = (input, message) => {
+    //get the form-field element 
+    const formField = input.parentElement;
+    // add the error class
+    formField.classList.remove('success');
+    formField.classList.add('error');
+    // show the error message
+    const error = formField.querySelector('small');
+    error.textContent = message;
+};
+const showSuccess = (input) => {
+    // get the form field element
+    const formField = input.parentElement:
+    // remove the error class
+    formField.classList.remove('error');
+    formField.classList.add('success');
+    // hide the error message
+    const error = formField.querySelector('small');
+    error.textContent = '';
+};
+
+// Input field validating function
+
+// Validate the username field
+const checkUsername = () => {
+    let valid = false;
+    const min = 3;
+    const max = 25;
+    const usernameEl = username.value.trim();
+
+    if (!isRequired(usernameEl)){
+        showError(username, 'Username cannot be blank');
+    }else if (!isBetween(usernameEl.length, min, max)){
+        showError(username, `Username must be between ${min} and ${max}`)
+    }else {
+        showSuccess(username);
+        valid = true;
+    }
+    return valid;
+}
+
+
 
 togglePassword.addEventListener('click', function(e){
     const type =  password.getAttribute('type') === 'password' ? 'text' : 'password';
@@ -15,3 +84,9 @@ toggleConfirmPassword.addEventListener('click', function(e){
     confirmPassword.setAttribute('type', confirmType);
     this.classList.toggle('bi-eye');
 });
+
+// function isRequired(value){
+//     return value ===''?false:true;
+// }
+
+// console.log(isRequired('dadfsfg'))
